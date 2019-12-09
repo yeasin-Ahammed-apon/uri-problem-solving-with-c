04455660a37703bd1f2783d6b6b1a5b44c566a14
yeasin-ahammed-apon <yeasinahammedapon@gmail.com>
2019-12-09 22:56:49 +0600

    1006

--- ap.c
+++ ap.c
@@ -1,10 +1,11 @@
 //Uri
 //1005
-#include<stdio.h>
-int main(){
-   float A,B,med;
-   scanf("%f %f",&A,&B);
-   med=((A*3.5)+(B*7.5))/(3.5+7.5);
-   printf("MEDIA = %.5f\n",med);
-   return 0;
+#include <stdio.h>
+int main()
+{
+ double a, b, c, media;
+ scanf("%lf%lf%lf", &a, &b, &c);
+ media = (a/10 * 2) + (b/10 * 3) + (c/10 * 5);
+ printf("MEDIA = %.1lf\n", media);
+ return 0;
 }
