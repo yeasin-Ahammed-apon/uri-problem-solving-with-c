6697aa7aacffb4ead67110b733edb8fc9a47f4aa
yeasin-ahammed-apon <yeasinahammedapon@gmail.com>
2019-12-09 21:22:36 +0600

    1002

--- ap.c
+++ ap.c
@@ -1,10 +1,10 @@
 //Uri
-//1001
+//1002
 #include<stdio.h>
 int main(){
-int A,B,X;
-scanf("%d%d",&A,&B);
-X=A+B;
-printf("%d",X);
+double A,n=3.14159,R;
+scanf("%lf",&R);
+A=n*(R*R);
+printf("A=%.4lf\n",A);
     return 0;
 }
