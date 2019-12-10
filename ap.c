1d7c52833ccb3e23f1bafb73a71e8a1126e430da
yeasin-ahammed-apon <yeasinahammedapon@gmail.com>
2019-12-10 23:08:03 +0600

    1015

--- ap.c
+++ ap.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
+#include<math.h>
 int main(){
-int X;
-float Y,K;
-scanf("%d%f",&X,&Y);
-K=X/Y;
-printf("%.3f km/l\n",K);//kilometer per liter
+double x1,y1,x2,y2,a,b;
+scanf("%lf%lf",&x1,&y1);
+scanf("%lf%lf",&x2,&y2);
+a=x2-x1;
+b=y2-y1;
+printf("%.4lf\n",sqrt((a*a)+(b*b)));
 return 0;
 }
