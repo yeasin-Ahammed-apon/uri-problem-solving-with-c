f79f0fcd25dba69d2bb3566b076712e1249830d1
yeasin-ahammed-apon <yeasinahammedapon@gmail.com>
2019-12-10 21:03:46 +0600

    1011

--- ap.c
+++ ap.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 int main()
 {
- int a, b;
- double c, res;
- scanf("%d %d %lf", &a, &b, &c);
- res = b * c;
- scanf("%d %d %lf", &a, &b, &c);
- res += b * c;
- printf("VALOR A PAGAR: R$ %.2lf\n", res);
+    //1011
+ double  pi=3.14159,R,vol;
+ scanf("%lf",&R);
+ vol =(4/3.0)*pi*R*R*R;
+ printf("VOLUME = %.3lf\n",vol);
  return 0;
 }
