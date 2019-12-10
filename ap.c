9e0a3ae6099a44405fa115841616fe8e89918849
yeasin-ahammed-apon <yeasinahammedapon@gmail.com>
2019-12-10 22:37:10 +0600

    1013

--- ap.c
+++ ap.c
@@ -1,14 +1,10 @@
-#include <stdio.h>
-int main()
-{
-    //1012
- float tri,cir,trap,quad,ret,A,B,C;
- scanf("%f%f%f",&A,&B,&C);
- tri=0.5*(A*C);
- cir=3.14159*(C*C);
- trap=0.5*(A+B)*C;
- quad=(B*B);
- ret=(A*B);
- printf("TRIANGULO: %.3f\nCIRCULO: %.3f\nTRAPEZIO: %.3f\nQUADRADO: %.3f\nRETANGULO: %.3f\n",tri,cir,trap,quad,ret);
- return 0;
+#include<stdio.h>
+int main(){
+int a,b,c,ab,abc;
+scanf("%d %d %d",&a,&b,&c);
+ab=(a+b+abs(a-b))/2;
+abc=(ab+c+abs(ab-c))/2;
+printf("%d eh o maior\n",abc);
+return 0;
+
 }
