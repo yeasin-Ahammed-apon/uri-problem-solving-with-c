0b0a61bb82ed4bb20e274e7374ee8a2fe2a7e133
yeasin-ahammed-apon <yeasinahammedapon@gmail.com>
2019-12-10 21:32:25 +0600

    1012

--- ap.c
+++ ap.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 int main()
 {
-    //1011
- double  pi=3.14159,R,vol;
- scanf("%lf",&R);
- vol =(4/3.0)*pi*R*R*R;
- printf("VOLUME = %.3lf\n",vol);
+    //1012
+ float tri,cir,trap,quad,ret,A,B,C;
+ scanf("%f%f%f",&A,&B,&C);
+ tri=0.5*(A*C);
+ cir=3.14159*(C*C);
+ trap=0.5*(A+B)*C;
+ quad=(B*B);
+ ret=(A*B);
+ printf("TRIANGULO: %.3f\nCIRCULO: %.3f\nTRAPEZIO: %.3f\nQUADRADO: %.3f\nRETANGULO: %.3f\n",tri,cir,trap,quad,ret);
  return 0;
 }
