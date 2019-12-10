a809dbb3e37915df6fe52efa9bb5f7e195f4d867
yeasin-ahammed-apon <yeasinahammedapon@gmail.com>
2019-12-11 00:01:13 +0600

    1016

--- ap.c
+++ ap.c
@@ -1,11 +1,7 @@
 #include<stdio.h>
-#include<math.h>
 int main(){
-double x1,y1,x2,y2,a,b;
-scanf("%lf%lf",&x1,&y1);
-scanf("%lf%lf",&x2,&y2);
-a=x2-x1;
-b=y2-y1;
-printf("%.4lf\n",sqrt((a*a)+(b*b)));
+int Y;
+scanf("%d",&Y);
+printf("%d minutos\n",2*Y);
 return 0;
 }
