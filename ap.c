53712990e13ea04d82c7790d6edb6487a567eab8
yeasin-ahammed-apon <yeasinahammedapon@gmail.com>
2019-12-12 20:32:42 +0600

    1018

--- ap.c
+++ ap.c
@@ -1,9 +1,16 @@
 #include<stdio.h>
+//1018
 int main(){
-int h,km;
-float l;
-scanf("%d%d",&h,&km);
-l=h*km;
-printf("%.3f\n",l/12);
+int m,n,i=0,k;
+int a[7]={100,50,20,10,5,2,1};
+scanf("%d",&k);
+printf("%d\n",k);
+while( i<7){
+        m=k/a[i];
+        n=k%a[i];
+        k=n;
+        printf("%d nota(s) de R$ %d,00\n",m,a[i]);
+}
+
 return 0;
 }
