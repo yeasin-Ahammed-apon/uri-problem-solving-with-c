eeb121fa2a3ec99b40ba94aca6bd10d7b1a8e379
apon <yeasinahammedapon@gmail.com>
2019-12-15 00:07:42 +0600

    1042

--- ap.c
+++ ap.c
@@ -1,26 +1,10 @@
 #include<stdio.h>
 int main(){
-int X,Y;
-float l;
-scanf("%d%d",&X,&Y);
-if(X==1){
-        l=Y*4.00;
-    printf("Total: R$ %.2f\n",l);
-}else if(X==2){
-l=Y*4.50;
-    printf("Total: R$ %.2f\n",l);
-}
-else if(X==3){
-l=Y*5.00;
-    printf("Total: R$ %.2f\n",l);
-}
-else if(X==4){
-l=Y*2.00;
-    printf("Total: R$ %.2f\n",l);
-}
-else if(X==5){
-l=Y*1.50;
-    printf("Total: R$ %.2f\n",l);
-}
+int a,b,c,f,s,t;
+scanf("%d%d%d",&a,&b,&c);
+if(a<b&&a<c){f=a;if(b<c){s=b; t=c;}else{s=c; t=b;}}
+else if(b<a&&b<c){f=b;if(a<c){s=a; t=c;}else{s=c; t=a;}}
+else if(c<a&&c<b){f=c;if(b<a){s=b; t=a;}else{s=a; t=b;}}
+printf("%d\n%d\n%d\n\n%d\n%d\n%d\n",f,s,t,a,b,c);
 return 0;
 }
