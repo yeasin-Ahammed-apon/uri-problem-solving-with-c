811b68f800947889bb9f18803863ea526ff692f3
apon <yeasinahammedapon@gmail.com>
2019-12-16 17:25:16 +0600

    1047

--- ap.c
+++ ap.c
@@ -1,17 +1,31 @@
 #include<stdio.h>
 int main()
 {
-    int a,b;
-    scanf("%d%d",&a,&b);
+    int a,c,b,d;
+    scanf("%d%d%d%d",&a,&c,&b,&d);
     if(a>b){
         a=(24-a);
         b=a+b;
-        printf("O JOGO DUROU %d HORA(S)\n",b);
+        if(c>d){
+          c=(60-c);
+            d=c+d;
+            printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)\n",b,d);
+        }else if(d>c){
+            d=d-c;
+            printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)\n",b,d);
+        }
     }else if(b>a){
     b=b-a;
-    printf("O JOGO DUROU %d HORA(S)\n",b);
+    if(c>d){
+          c=(60-c);
+            d=c+d;
+            printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)\n",b,d);
+        }else if(d>c){
+            d=d-c;
+            printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)\n",b,d);
+        }
     }else{
-    printf("O JOGO DUROU %d HORA(S)\n",24);
+    printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)\n",24,0);
     }
-
+return 0;
 }
