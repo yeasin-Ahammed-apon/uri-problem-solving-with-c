89578e8670b426c3bc9e722b2232572fe257d3da
apon <yeasinahammedapon@gmail.com>
2019-12-16 23:00:34 +0600

    1061

--- ap.c
+++ ap.c
@@ -1,14 +1,25 @@
 #include<stdio.h>
 int main(){
-float a,b,c,d,e,f;
-int p;
-scanf("%f%f%f%f%f%f",&a,&b,&c,&d,&e,&f);
-if(a>0){++p;}
-if(b>0){++p;}
-if(c>0){++p;}
-if(d>0){++p;}
-if(e>0){++p;}
-if(f>0){++p;}
-printf("%d valores positivos\n",p);
+int d1,h1,m1,s1,d2,h2,m2,s2,rd,rh,rm,rs;
+scanf("%d%d%d%d%d%d%d%d",&d1,&h1,&m1,&s1,&d2,&h2,&m2,&s2);
+rd=d2-d1;
+rh=(24-h1)+h2;
+rm=(60-m1)+m2;
+rs=(60-s1)+s1;
+rd=(rd-1)*24*60*60;
+rh=rh*60*60;
+rm=rm*60;
+rs;
+rs=rs+rd+rh+rm;
+rd=rs/86400;
+rs=rs%86400;
+rh=rs/3600;
+rs=rs%3600;
+rm=rs/60;
+rs=rs%60;
+printf("%d dia(s)\n",rd);
+printf("%d hora(s)\n",rh);
+printf("%d minuto(s)\n",rm);
+printf("%d segundo(s)\n",rs);
 return 0;
 }
