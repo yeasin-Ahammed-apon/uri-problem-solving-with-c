d7df9711dad9100e1ce01570445138bfecdc1caa
apon <yeasinahammedapon@gmail.com>
2019-12-16 20:50:52 +0600

    1060

--- ap.c
+++ ap.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
 int main(){
-int i;
-for(i=1;i<=100;i++){
-    if(i%2==0){
-       printf("%d\n",i);
-    }
-}
+float a,b,c,d,e,f;
+int p;
+scanf("%f%f%f%f%f%f",&a,&b,&c,&d,&e,&f);
+if(a>0){++p;}
+if(b>0){++p;}
+if(c>0){++p;}
+if(d>0){++p;}
+if(e>0){++p;}
+if(f>0){++p;}
+printf("%d valores positivos\n",p);
 return 0;
 }
