896bd863e02b3fba0aded58b92376dc342742d03
apon <yeasinahammedapon@gmail.com>
2019-12-16 20:16:20 +0600

    1052

--- ap.c
+++ ap.c
@@ -1,25 +1,48 @@
 #include<stdio.h>
-int main(){
-int a ;
-scanf("%d",&a);
-if(a==61){
-    printf("Brasilia\n");
-}else if(a==71){
-printf("Salvador\n");
-}else if(a==11){
-printf("Sao Paulo\n");
-}else if(a==21){
-printf("Rio de Janeiro\n");
-}else if(a==32){
-printf("Juiz de Fora\n");
-}else if(a==19){
-printf("Campinas\n");
-}else if(a==27){
-printf("Vitoria\n");
-}else if(a==31){
-printf("Belo Horizonte\n");
-}else{
-printf("DDD nao cadastrado\n");
-}
-return 0;
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    switch (n)
+    {
+    case 1:
+        printf("January\n");
+        break;
+    case 2:
+        printf("February\n");
+        break;
+    case 3:
+        printf("March\n");
+        break;
+    case 4:
+        printf("April\n");
+        break;
+    case 5:
+        printf("May\n");
+        break;
+    case 6:
+        printf("June\n");
+        break;
+    case 7:
+        printf("July\n");
+        break;
+    case 8:
+        printf("August\n");
+        break;
+    case 9:
+        printf("September\n");
+        break;
+    case 10:
+        printf("October\n");
+        break;
+    case 11:
+        printf("November\n");
+        break;
+    case 12:
+        printf("December\n");
+        break;
+
+    default:
+        break;
+    }
 }
