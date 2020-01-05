a96985ea4849a1d448e61b3dddc8d066b4599efd
yeasin_ahammed_apon <yeasinahammedapon@gmail.com>
2020-01-05 15:40:31 +0600

    1078

--- ap.c
+++ ap.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main(){
 int n, i;
-	scanf("%d",&n);
+scanf("%d",&n);
+	for ( i = 1; i <= 10; i++ )
+		printf("%d x %d = %d\n",i,n,i*n);
 
-	for ( i = 1; i < 10000; i += n )
-        printf("%d\n",i);
     return 0;
 }
