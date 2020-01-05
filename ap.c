88743954ffae5b8f6ddee4e076205d31848edabd
yeasin_ahammed_apon <yeasinahammedapon@gmail.com>
2020-01-05 15:25:19 +0600

    1075

--- ap.c
+++ ap.c
@@ -1,23 +1,9 @@
 #include <stdio.h>
 int main(){
-int n, i, num;
+int n, i;
 	scanf("%d",&n);
-	for ( i = 0; i < n; i++ ){
-		scanf("%d",&num);
-		if ( num == 0 ){
-			printf("NULL\n");
-		}else{
-			if( num % 2 ){
-			    printf("ODD");
-				}else{
-                printf("EVEN");
-			}
-			if ( num > 0 ){
-                printf(" POSITIVE\n");
-			}else{
-                printf(" NEGATIVE\n");
-			}
-		}
-	}
+
+	for ( i = 1; i < 10000; i += n )
+        printf("%d\n",i);
     return 0;
 }
