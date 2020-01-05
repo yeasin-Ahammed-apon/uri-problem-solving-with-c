b73c7b70c79441e8347efa9cea01430228d9ba60
yeasin_ahammed_apon <yeasinahammedapon@gmail.com>
2020-01-05 12:43:19 +0600

    1067

--- ap.c
+++ ap.c
@@ -1,16 +1,9 @@
 #include <stdio.h>
-int main(){
-    int i, odd = 0,even = 0, pos = 0, neg = 0, num;
-	for ( i = 0; i < 5; i++ )	{
-		scanf("%d",&num);
-		if ( num % 2 == 0 ){even++;}
-		else {odd++;}
-		if ( num > 0 ) pos++;
-		if ( num < 0 ) neg++;
-	}
-	printf("%d valor(es) par(es)\n",even);
-	printf("%d valor(es) impar(es)\n",odd);
-	printf("%d valor(es) positivo(s)\n",pos);
-    printf("%d valor(es) negativo(s)\n",neg);
+int main()
+{
+	int n, i;
+	scanf("%d",&n);
+	for ( i = 1; i <= n; i += 2 )
+		printf("%d\n",i);
 	return 0;
 }
