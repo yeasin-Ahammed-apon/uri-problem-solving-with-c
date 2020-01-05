f1f992eff015f550dab1f50c52ac475863821693
yeasin_ahammed_apon <yeasinahammedapon@gmail.com>
2020-01-05 12:48:32 +0600

    1070

--- ap.c
+++ ap.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-int main()
-{
+int main(){
 	int n, i;
 	scanf("%d",&n);
-	for ( i = 1; i <= n; i += 2 )
+	n = ( n % 2 ) ? n : n + 1;
+	for ( i = n; i <= n + 10; i += 2 )
 		printf("%d\n",i);
 	return 0;
 }
